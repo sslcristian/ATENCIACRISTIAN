8354ce69a81e4f2f14544b23af3a4e065c87eafd
Andrés Felipe Jerez <anfejear@hotmail.com>
2025-03-27 14:35:38 -0500

    Merging

--- javaFXProject/src/application/Main.java
+++ javaFXProject/src/application/Main.java
@@ -38,7 +38,7 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
-	//public static void main(String[] args) {
-	//	launch(args);
-	//}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
